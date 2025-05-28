031fb752c89c6cabe3e8669d9abfbec7b84e2383
SurPrise25 <djafarjustin@gmail.com>
2025-05-28 18:55:45 +0700

    Up

---
+++ script.js
@@ -0,0 +1,20 @@
+const glow = document.getElementById('glow');
+
+window.addEventListener('mousemove', (e) => {
+  glow.style.left = e.clientX + 'px';
+  glow.style.top = e.clientY + 'px';
+});
+
+const githubBtn = document.getElementById('github-btn');
+
+githubBtn.addEventListener('click', () => {
+  window.open('https://github.com/JustinDjafar', '_blank');
+});
+
+githubBtn.addEventListener('mouseenter', () => {
+  githubBtn.style.transform = 'translate(-50%, -50%) scale(1.1)';
+});
+
+githubBtn.addEventListener('mouseleave', () => {
+  githubBtn.style.transform = 'translate(-50%, -50%) scale(1)';
+});
